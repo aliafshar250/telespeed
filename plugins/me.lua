dafab34ecb0fd2ac5364b3fecaad7fee37bc2eda
mehdi <mehdizare863@yahoo.com>
2016-04-03 00:57:17 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -3,19 +3,19 @@ do
 local function run(msg, matches)
   if matches[1]:lower() == 'me' or "wai" then
     if is_sudo(msg) then
-    send_document(get_receiver(msg), "./data/me/sudo.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/sudo.webp", ok_cb, false)
       return "Your : SuDo"
     elseif is_admin(msg) then
-    send_document(get_receiver(msg), "./data/me/admins.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/admins.webp", ok_cb, false)
       return "Your : Admins"
     elseif is_owner(msg) then
-    send_document(get_receiver(msg), "./data/me/owner.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/owner.webp", ok_cb, false)
       return "Your : Owner"
     elseif is_momod(msg) then
-    send_document(get_receiver(msg), "./data/me/mod.webp", ok_cb, false)
+    send_document(get_receiver(msg), "./data/mods.webp", ok_cb, false)
       return "Your : Mods"
     else
-      send_document(get_receiver(msg), "./data/me/member.webp", ok_cb, false)
+      send_document(get_receiver(msg), "./data/member.webp", ok_cb, false)
       return "You : member"
   end
 end
